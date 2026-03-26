ae4acd919110a0d4be9f18325fa611bcabefc988
babo.neo <seirion@mensakorea.org>
2026-03-26 20:53:08 +0900

    로그인 화면 타이틀 '참교육 K' 로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ function App() {
         </div>
       ) : (
         <div className="login">
-          <h1>케이</h1>
+          <h1>참교육 K</h1>
           <button className="btn btn-google" onClick={signInWithGoogle}>
             <svg viewBox="0 0 24 24" width="20" height="20">
               <path fill="#4285F4" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"/>
